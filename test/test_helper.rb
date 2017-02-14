80171bb5fd317bf7fe851152c5abba46acce8776
Wyatt Kirby <wyatt@apsis.io>
2017-02-14 10:37:45 -0800

    rubocop fixes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,7 +8,7 @@ require 'action_controller'
 Dir['test/assertions/**/*.rb'].each { |f| require File.expand_path(f) }
 Dir['test/fixtures/**/*.rb'].each { |f| require File.expand_path(f) }
 
-system("bundle exec rake db:migrate")
+system('bundle exec rake db:migrate')
 
-db_config = YAML.load(File.open("test/config/database.yml"))
+db_config = YAML.load(File.open('test/config/database.yml'))
 ActiveRecord::Base.establish_connection(db_config)
