dda89aec3e3053e114f21257ec9ea27074e8f105
Noah Callaway <noah@apsis.io>
2017-03-10 22:12:11 -0800

    Fix command template

--- lib/generators/command/templates/command.rb
+++ lib/generators/command/templates/command.rb
@@ -1,13 +1,17 @@
 <% module_namespacing do -%>
 class <%= class_name %>Command < Slayer::Command
-  def call(<%= file_name %>_form: <%= file_name %>_form)
-    fail! unless test_form.kind_of?(<%= class_name %>Form) && <%= file_name %>_form.valid?
+  def call(<%= file_name %>_form:)
+    fail! unless arguments_valid?(<%= file_name %>_form)
 
     transaction do
       # Do your database interaction here!
     end
 
     pass!
+  end
+
+  def arguments_valid?(<%= file_name %>_form)
+    <%= file_name %>_form.kind_of?(<%= class_name %>Form) && <%= file_name %>_form.valid?
   end
 end
 <% end -%>
