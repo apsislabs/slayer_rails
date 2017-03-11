83bf45b5ffd2a48418da5de5960cfdd863b933a7
Noah Callaway <noah@apsis.io>
2017-03-10 18:03:03 -0800

    Fix form generator tests and build

--- test/lib/generators/form_generator_test.rb
+++ test/lib/generators/form_generator_test.rb
@@ -4,14 +4,16 @@ class SlayerRails::FormGeneratorTest < Rails::Generators::TestCase
   tests Slayer::Generators::FormGenerator
   destination File.expand_path("../../sample/tmp", File.dirname(__FILE__))
 
-  arguments %w(Test)
+
   def test_form_skeleton_is_created
+    self.class.arguments %w(Test)
     run_generator
     assert_file "app/forms/test_form.rb", /class TestForm < Slayer::Form/
   end
 
-  arguments %w(FieldTest name:String age:Integer comment other)
+
   def test_form_attributes_are_created
+    self.class.arguments %w(FieldTest name:String age:Integer comment other)
     run_generator
     assert_file "app/forms/field_test_form.rb" do |contents|
       assert_match(/class FieldTestForm < Slayer::Form/, contents)
