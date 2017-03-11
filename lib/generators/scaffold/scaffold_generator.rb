40b4fe22d74205e48532fe2f7ec5e0dcafe8bfde
Noah Callaway <noah@apsis.io>
2017-03-10 17:57:36 -0800

    Scaffold generator

--- lib/generators/scaffold/scaffold_generator.rb
+++ lib/generators/scaffold/scaffold_generator.rb
@@ -1,3 +1,7 @@
+def genlog(str)
+  `echo "#{str}" >> genlog`
+end
+
 module Slayer
   module Generators
     class ScaffoldGenerator < NamedBase
@@ -6,12 +10,26 @@ module Slayer
       source_root File.expand_path("../templates", __FILE__)
       check_class_collision suffix: "Command"
 
+      argument :name, type: :string
+      argument :fields, :type => :array, :required => false, :desc => "The attributes of the generated form. name:String completed:Boolean"
+
+      def initialize(args, *options) #:nodoc:
+        genlog "s1: #{args.inspect}"
+        super
+        genlog "s2: #{args.inspect}"
+        @args = args
+        genlog "s3: #{@args.inspect}"
+      end
+
       def create_command_files
         generate "slayer:command", "#{file_name}"
       end
 
       def create_form_files
-        generate "slayer:form", "#{file_name}"
+        genlog "slayer:scaffold:create_form_files"
+        genlog "#{@args.inspect}"
+        genlog "#{@args.join(" ")}"
+        generate "slayer:form", "#{@args.join(" ")}"
       end
     end
   end
