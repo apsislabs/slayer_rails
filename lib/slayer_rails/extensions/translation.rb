f7b8eef2a8dfbb7c1801e21e6a47c875f4dc051e
Chris Pfohl <christopher.pfohl@gmail.com>
2018-11-13 16:03:45 -0500

    Make Slayer Type detection more robust

--- lib/slayer_rails/extensions/translation.rb
+++ lib/slayer_rails/extensions/translation.rb
@@ -30,14 +30,33 @@ module SlayerRails
 
 
           def build_implied_path
-            module_path = self.name.split('::').map { |x| x.underscore }
+            module_path = self.name.underscore.split('/')
 
             class_name_parts = module_path.pop.split('_')
 
-            module_path.unshift(class_name_parts.pop.pluralize)
+            if ['command', 'service', 'form'].include? class_name_parts.last
+              class_name_parts.pop
+            end
+
+            module_path.unshift(slayer_type)
             module_path.push(class_name_parts.join('_'))
 
             module_path.join('.')
+          end
+
+          def slayer_type
+            @slayer_type ||= detect_slayer_type
+          end
+
+          def detect_slayer_type
+            if self <= Slayer::Command
+              return 'commands'
+            elsif self <= Slayer::Service
+              return 'services'
+            elsif self <= Slayer::Form
+              return 'forms'
+            end
+            raise NotImplementedException, "Unknown Slayer Class: #{self.name}"
           end
         end
       end
